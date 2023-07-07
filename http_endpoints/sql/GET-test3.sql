d149e85abe51b151633bfd0f96934be663fda736
sanshuiyijing <jingdeveloper@126.com>
2023-07-07 10:41:52 +0800

    add endpoint

--- http_endpoints/sql/GET-test3.sql
+++ http_endpoints/sql/GET-test3.sql
@@ -3,4 +3,5 @@ Enter "USE {database};" before entering your SQL statements.
 Type "--your question" + Enter to try out AI-generated SQL queries
 Declare a parameter like "Where id = ${arg}".
 */
-SELECT "Hello World1";
+SELECT "Hello World12";
+-- add comment
